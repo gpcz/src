29ea278b6e8d366735c6ae3dccba1aeea286258d
Greg Czerniak <greg@czerniak.info>
2017-12-05 23:58:49 -0500

    Some more progress.

--- sys/arch/armv7/atmel/atmlaic.c
+++ sys/arch/armv7/atmel/atmlaic.c
@@ -13,10 +13,15 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+/*
+ * Device driver for the Atmel Advanced Interrupt Controller (AIC).
+ */
 
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/malloc.h>
 #include <sys/device.h>
+#include <sys/evcount.h>
 
 #include <machine/bus.h>
 #include <machine/fdt.h>
@@ -27,6 +32,8 @@
 #define AIC_SSR			0x000 /* Source Select Register */
 #define AIC_SMR			0x004 /* Source Mode Register */
 #define AIC_SVR			0x008 /* Source Vector Register */
+#define AIC_EOICR		0x038 /* End of Interrupt Command Register*/
+#define AIC_SPU			0x03C /* Spurious Interrupt Vector Register */
 #define AIC_IDCR		0x044 /* Interrupt Disable Command Register */
 /******************************************************************************/
 
@@ -35,6 +42,7 @@ struct atmlaic_softc {
 	bus_space_tag_t			sc_iot;
 	bus_space_handle_t		sc_ioh;
 	int				sc_node;
+	struct evcount		 	sc_spur;
 	struct interrupt_controller 	sc_intc;
 };
 
@@ -42,6 +50,7 @@ int		 atmlaic_match(struct device *, void *, void*);
 void		 atmlaic_attach(struct device *, struct device *, void*);
 void		*atmlaic_intr_establish(void *, int *, int,
 		    int (*)(void *), void *, char *);
+void		 atmlaic_intr(void);
 
 struct cfattach atmlaic_ca = {
 	sizeof (struct atmlaic_softc), atmlaic_match, atmlaic_attach
@@ -65,18 +74,34 @@ atmlaic_attach(struct device *parent, struct device *self, void *args)
 	struct atmlaic_softc *sc = (struct atmlaic_softc *)self;
 	struct fdt_attach_args *faa = args;
 
+	/*
+	 * There is only one region of memory-mapped register space for
+	 * this device.  If there's zero or more than one in the device
+	 * tree file, something is wrong.
+	 */
 	if (faa->fa_nreg != 1)
 		panic("%s: number of atmlaic registers != 1!", __func__);
 	sc->sc_node = faa->fa_node;
 	sc->sc_iot = faa->fa_iot;
-	
+
+	/* Get access to the SoC's memory-mapped space for this device. */
 	if (bus_space_map(sc->sc_iot, faa->fa_reg[0].addr,
 	    faa->fa_reg[0].size, 0, &sc->sc_ioh))
 		panic("%s: bus_space_map failed!", __func__);
 
+	/*
+	 * Set up the spurious interrupt service routine and make an
+	 * evcount to track how often spurious interrupts happen.
+	 */
+	evcount_attach(&sc->sc_spur, "spur", NULL);
+	bus_space_write_4(sc->sc_iot, sc->sc_ioh, AIC_SPU,
+	    (u_int32_t)atmlaic_spur_intr);
+
+	/* Register this driver as an interrupt controller to the OS. */
 	sc->sc_intc.ic_node = faa->fa_node;
 	sc->sc_intc.ic_cookie = sc;
 	sc->sc_intc.ic_establish = atmlaic_intr_establish;
+	sc->sc_intc.ic_disestablish = atmlaic_intr_disestablish;
 	arm_intr_register_fdt(&sc->sc_intc);
 }
 
@@ -86,4 +111,35 @@ atmlaic_intr_establish(void *cookie, int *cells, int level,
 {
 	struct intrhand		*ih = NULL;
 	return ih;
+}
+
+void
+atmlaic_intr_disestablish(void *cookie)
+{
+}
+
+void
+atmlaic_intr(void)
+{
+	/* Get the IRQ number from the AIC. */
+	/* Get the interrupt information from the array. */
+	/* Call the interrupt function and increment evcount if return > 0. */
+	/*
+	 * Set the End of Interrupt register.  This jumps us back to what we
+	 * were doing.
+	 */
+	bus_space_write_4(sc->sc_iot, sc->sc_ioh, AIC_EOICR, i);
+}
+
+void
+atmlaic_spur_intr(void)
+{
+	/* Increment the spurious interrupt counter. */
+	sc->sc_spur.ec_count++;
+	
+	/*
+	 * Set the End of Interrupt register.  This jumps us back to what we
+	 * were doing.
+	 */
+	bus_space_write_4(sc->sc_iot, sc->sc_ioh, AIC_EOICR, i);
 }
